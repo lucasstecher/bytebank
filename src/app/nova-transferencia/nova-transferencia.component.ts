5188ff186924529c7f7f05c244e11a929f7a3eee
Lucas Stecher <lucas.oliveira@coopersystem.com.br>
2021-08-10 00:17:05 -0300

    cration of nova-transferencia and extrato modules

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent implements OnInit {
+
+  valor!: number;
+  destino!: number;
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  transferir() {
+    console.log('Solicitada transferência!');
+    const valorEmitir = { valor: this.valor, destino:this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
